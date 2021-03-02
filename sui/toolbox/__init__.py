31afcdaaab7eabbe10e7d287b1908138747f1eb3
l-tang <litang1025@gmail.com>
2021-03-02 14:26:17 +0800

    Add build_session into toolbox.

--- sui/toolbox/__init__.py
+++ sui/toolbox/__init__.py
@@ -1,5 +1,6 @@
 """sui.toolbox
-Tool kits for data science
+Tool kits for data processing
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 from .top_k import top_k
+from .build_session import build_session
