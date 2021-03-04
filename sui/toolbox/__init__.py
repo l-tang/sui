90b6b9fe5b9d181195914ef9e2c1660dd7cf7d30
l-tang <litang1025@gmail.com>
2021-03-04 20:07:17 +0800

    Add random_walk into toolbox.

--- sui/toolbox/__init__.py
+++ sui/toolbox/__init__.py
@@ -3,4 +3,4 @@ Tool kits for data processing
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
 from .top_k import top_k
-from .build_session import build_session
+from .build_series_data import build_session, random_walk
